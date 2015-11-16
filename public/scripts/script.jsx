9c36c700d91bd78d92538490e0442bf2765b639e
aequanimitas <htavenido@gmail.com>
2015-11-16 17:43:28 +0800

    add leaflet map inside react

--- public/scripts/script.jsx
+++ public/scripts/script.jsx
@@ -1,34 +1,60 @@
-    // lat: 14.60966442647526, lng: 121.10607147216795
-var kvCoords = [14.60966442647526, 121.10607147216795]
-var map = L.map('map').setView(kvCoords, 18);
-var attribution = "<a href='http://openstreetmap.org'>OpenStreetMap</a>";
-L.tileLayer('http://localhost/osm_tiles/{z}/{x}/{y}.png', {
-  attribution: attribution,
-  maxZoom: 20
-}).addTo(map);
- 
-var courts = {
-  "1A": { 
-    "coords": [14.611128274679167, 121.10397934913634] 
-   },
-  "1B": { 
-    "coords": [14.60638890103478, 121.10630750656127]
-   },
-  "1C": { 
-    "coords": [14.6087923420603, 121.10480010509491]
-   },
-  "Kalayaan": { 
-    "coords": [14.60823690554013, 121.10714972019196]
+var MapBox = React.createClass({
+  setupCourtMarkers: function() {
+    var courts = [
+      { 
+        "name": "1A",
+        "coords": [14.611128274679167, 121.10397934913634] 
+      },
+      {
+        "name": "1B",
+        "coords": [14.60638890103478, 121.10630750656127]
+      },
+      { 
+        "name": "1C",
+        "coords": [14.6087923420603, 121.10480010509491]
+      },
+      {
+        "name": "Kalayaan",
+        "coords": [14.60823690554013, 121.10714972019196]
+      }
+    ];
+    var markers = []; 
+    courts.forEach(function(x) {
+      var markerInfo = `${x.name} Court`;
+      var marker = L.marker(x.coords,{
+          "title": markerInfo,
+          "riseOnHover": true
+        })
+        .bindPopup(`<b>${markerInfo}</b>`)
+        .addTo(this.map);
+      
+      marker.on('click', function(e) {
+        // render a div inside react
+        console.dir(e.target.options.title);
+      });
+      markers.push(marker);
+    }, this);
+  },
+  buildMap: function() {
+    var kvCoords = [14.60966442647526, 121.10607147216795]
+    this.map.setView(kvCoords, 18);
+    L.tileLayer('http://localhost/osm_tiles/{z}/{x}/{y}.png', {
+      maxZoom: 20
+    }).addTo(this.map);
+    this.setupCourtMarkers();
+  },
+  componentDidMount: function() {
+    // add these as children??
+    // var x = window.document.getElementsByClassName('leaflet-marker-icon');
+    this.map = L.map(ReactDOM.findDOMNode(this));
+    this.buildMap();
+  },
+  render: function() {
+    return (<div id="map"></div>);
   }
-}
-
-Array.prototype.forEach.call(Object.keys(courts), function(x) {
-  L.marker(courts[x].coords)
-    .bindPopup(`<b>${x} Court</b>`)
-    .addTo(map);
 });
 
-// debug
-map.on('click', function(e) {
-  console.log(e.latlng);
-});
+ReactDOM.render(
+  <MapBox />,
+  document.getElementById('app')
+);
